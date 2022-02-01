174efe8b8eaf8d7902ded81df1416e0b7d6a2e20
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-02-01 02:29:20 +0100

    Add header component

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import { NavLink } from 'react-router-dom';
+import React from 'react';
+import planet from '../img/planet_images/planet.png';
+
+const Header = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/Missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/Profile',
+      text: 'My Profile',
+    },
+  ];
+  return (
+  <header>
+    <img src={planet} alt='logo' />
+    <nav>
+      {links.map((link) => (
+        <NavLink
+          key={link.id}
+          to={link.path}
+        >
+        {link.text}
+        </NavLink>
+      ))}
+      </nav>
+  </header>
+  );
+};
+
+export default Header;
