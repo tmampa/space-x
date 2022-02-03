9286a851a2e3ae99ddbd4f024f2a5a4306f7315a
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-02-03 01:11:34 +0100

    pass prop listTitle to ReservedItemsList component

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -5,7 +5,6 @@ import ReservedItemsList from '../components/profileComponents/ReservedItemsList
 const MyProfile = () => {
   const missionsList = useSelector((state) => state.missionsReducer);
   const filteredMissions = missionsList.filter((mission) => mission.missionReserved);
-  const unjoinedMissions = missionsList.filter((mission) => !mission.missionReserved);
 
   return (
     <div className="profile-page-container">
@@ -16,14 +15,7 @@ const MyProfile = () => {
           itemName: mission.missionName,
         }))}
       />
-      {/* <p>Reserved Rockets List here</p> */}
-      <ReservedItemsList
-        listTitle="Not joined Missions"
-        reservedItems={unjoinedMissions.map((mission) => ({
-          itemId: mission.missionId,
-          itemName: mission.missionName,
-        }))}
-      />
+      <p>Reserved Rockets List here</p>
     </div>
   );
 };
