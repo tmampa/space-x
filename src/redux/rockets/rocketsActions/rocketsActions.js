2502d1dd81a3649eb572a7c89d9a5ed5d64a3bf2
M0rrighan <81151384+M0rrighan@users.noreply.github.com>
2022-02-04 16:03:41 +0100

    Update README.md file

--- src/redux/rockets/rocketsActions/rocketsActions.js
+++ src/redux/rockets/rocketsActions/rocketsActions.js
@@ -1,5 +1,5 @@
-export const LIST_ROCKETS = 'spaceX/missions/LIST_ROCKETS';
-export const SET_ROCKET_RESERVATION = 'spaceX/missions/SET_ROCKET_RESERVATION';
+export const LIST_ROCKETS = 'spaceX/rockets/LIST_ROCKETS';
+export const SET_ROCKET_RESERVATION = 'spaceX/rockets/SET_ROCKET_RESERVATION';
 
 export const listRockets = (payload) => ({
   type: LIST_ROCKETS,
