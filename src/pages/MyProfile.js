b01cd80dfb949deeced26ae5bf0402afd87a7dda
tmampa <tmampa94@gmail.com>
2022-02-04 02:41:34 +0200

    Fix linter errors

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,33 +1,33 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import ReservedItemsList from "../components/profileComponents/ReservedItemsList";
+import React from 'react';
+import { useSelector } from 'react-redux';
+import ReservedItemsList from '../components/profileComponents/ReservedItemsList';
 
 const MyProfile = () => {
-	const missionsList = useSelector((state) => state.missionsReducer);
-	const filteredMissions = missionsList.filter(
-		(mission) => mission.missionReserved
-	);
-	const rocketsList = useSelector((state) => state.rocketsReducer);
-	const filteredRockets = rocketsList.filter((rocket) => rocket.rocketReserved);
+  const missionsList = useSelector((state) => state.missionsReducer);
+  const filteredMissions = missionsList.filter(
+    (mission) => mission.missionReserved,
+  );
+  const rocketsList = useSelector((state) => state.rocketsReducer);
+  const filteredRockets = rocketsList.filter((rocket) => rocket.rocketReserved);
 
-	return (
-		<div className="profile-page-container">
-			<ReservedItemsList
-				listTitle="My Missions"
-				reservedItems={filteredMissions.map((mission) => ({
-					itemId: mission.missionId,
-					itemName: mission.missionName,
-				}))}
-			/>
-			<ReservedItemsList
-				listTitle="My Rockets"
-				reservedItems={filteredRockets.map((rocket) => ({
-					itemId: rocket.rocketId,
-					itemName: rocket.rocketName,
-				}))}
-			/>
-		</div>
-	);
+  return (
+    <div className="profile-page-container">
+      <ReservedItemsList
+        listTitle="My Missions"
+        reservedItems={filteredMissions.map((mission) => ({
+				  itemId: mission.missionId,
+				  itemName: mission.missionName,
+        }))}
+      />
+      <ReservedItemsList
+        listTitle="My Rockets"
+        reservedItems={filteredRockets.map((rocket) => ({
+				  itemId: rocket.rocketId,
+				  itemName: rocket.rocketName,
+        }))}
+      />
+    </div>
+  );
 };
 
 export default MyProfile;
