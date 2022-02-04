f16e88b81ff110f1b88a912d7943d82c424b1562
tmampa <tmampa94@gmail.com>
2022-02-04 02:27:24 +0200

    Display joined missions in my profile

--- src/pages/MyProfile.js
+++ src/pages/MyProfile.js
@@ -1,23 +1,33 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
-import ReservedItemsList from '../components/profileComponents/ReservedItemsList';
+import React from "react";
+import { useSelector } from "react-redux";
+import ReservedItemsList from "../components/profileComponents/ReservedItemsList";
 
 const MyProfile = () => {
-  const missionsList = useSelector((state) => state.missionsReducer);
-  const filteredMissions = missionsList.filter((mission) => mission.missionReserved);
+	const missionsList = useSelector((state) => state.missionsReducer);
+	const filteredMissions = missionsList.filter(
+		(mission) => mission.missionReserved
+	);
+	const rocketsList = useSelector((state) => state.rocketsReducer);
+	const filteredRockets = rocketsList.filter((rocket) => rocket.rocketReserved);
 
-  return (
-    <div className="profile-page-container">
-      <ReservedItemsList
-        listTitle="My Missions"
-        reservedItems={filteredMissions.map((mission) => ({
-          itemId: mission.missionId,
-          itemName: mission.missionName,
-        }))}
-      />
-      <p>Reserved Rockets List here</p>
-    </div>
-  );
+	return (
+		<div className="profile-page-container">
+			<ReservedItemsList
+				listTitle="My Missions"
+				reservedItems={filteredMissions.map((mission) => ({
+					itemId: mission.missionId,
+					itemName: mission.missionName,
+				}))}
+			/>
+			<ReservedItemsList
+				listTitle="My Rockets"
+				reservedItems={filteredRockets.map((rocket) => ({
+					itemId: rocket.rocketId,
+					itemName: rocket.rocketName,
+				}))}
+			/>
+		</div>
+	);
 };
 
 export default MyProfile;
