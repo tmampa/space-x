7f0b91b14d561813958acd322339fba5f0feb7d7
tmampa <tmampa94@gmail.com>
2022-02-04 02:01:28 +0200

    Fix linter errors

--- src/components/rocketsComponents/RocketsList.js
+++ src/components/rocketsComponents/RocketsList.js
@@ -1,32 +1,32 @@
-import { useEffect } from "react";
-import { useDispatch, useSelector } from "react-redux";
-import fetchRockets from "../../redux/rockets/rocketsThunks/rocketsThunks";
-import RocketItem from "./RocketItem";
+import { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import fetchRockets from '../../redux/rockets/rocketsThunks/rocketsThunks';
+import RocketItem from './RocketItem';
 
 const RocketsList = () => {
-	const dispatch = useDispatch();
-	const rocketsList = useSelector((state) => state.rocketsReducer);
+  const dispatch = useDispatch();
+  const rocketsList = useSelector((state) => state.rocketsReducer);
 
-	useEffect(() => {
-		if (rocketsList.length < 1) {
-			dispatch(fetchRockets());
-		}
-	});
+  useEffect(() => {
+    if (rocketsList.length < 1) {
+      dispatch(fetchRockets());
+    }
+  });
 
-	return (
-		<ul>
-			{rocketsList.map((rocket) => (
-				<RocketItem
-					key={rocket.rocketId}
-					rocketId={rocket.rocketId}
-					rocketName={rocket.rocketName}
-					rocketDescription={rocket.rocketDescription}
-					rocketImageLink={rocket.rocketImages[0]}
-					rocketReserved={rocket.rocketReserved}
-				/>
-			))}
-		</ul>
-	);
+  return (
+    <ul>
+      {rocketsList.map((rocket) => (
+        <RocketItem
+          key={rocket.rocketId}
+          rocketId={rocket.rocketId}
+          rocketName={rocket.rocketName}
+          rocketDescription={rocket.rocketDescription}
+          rocketImageLink={rocket.rocketImages[0]}
+          rocketReserved={rocket.rocketReserved}
+        />
+      ))}
+    </ul>
+  );
 };
 
 export default RocketsList;
--- src/redux/rockets/rocketsActions/rocketsActions.js
+++ src/redux/rockets/rocketsActions/rocketsActions.js
@@ -1,12 +1,12 @@
-export const LIST_ROCKETS = "spaceX/missions/LIST_ROCKETS";
-export const SET_ROCKET_RESERVATION = "spaceX/missions/SET_ROCKET_RESERVATION";
+export const LIST_ROCKETS = 'spaceX/missions/LIST_ROCKETS';
+export const SET_ROCKET_RESERVATION = 'spaceX/missions/SET_ROCKET_RESERVATION';
 
 export const listRockets = (payload) => ({
-	type: LIST_ROCKETS,
-	payload,
+  type: LIST_ROCKETS,
+  payload,
 });
 
 export const setRocketReservation = (payload) => ({
-	type: SET_ROCKET_RESERVATION,
-	payload,
+  type: SET_ROCKET_RESERVATION,
+  payload,
 });
