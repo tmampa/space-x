d936a34758eddf8dbbe1765461c5827af80c7eb9
tmampa <tmampa94@gmail.com>
2022-02-05 11:22:49 +0200

    Fix linter errors

--- src/tests/Mission.test.js
+++ src/tests/Mission.test.js
@@ -1,28 +1,28 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import { render, screen } from "@testing-library/react";
-import { Provider } from "react-redux";
-import store from "../../src/redux/configureStore";
-import MissionsTable from "../../src/components/missionsComponents/MissionsTable";
+import React from 'react';
+import renderer from 'react-test-renderer';
+import { render, screen } from '@testing-library/react';
+import { Provider } from 'react-redux';
+import store from '../redux/configureStore';
+import MissionsTable from '../components/missionsComponents/MissionsTable';
 
-describe("Mission Element Tests", () => {
-	test("Check if Component Renders in DOM", () => {
-		render(
-			<Provider store={store}>
-				<MissionsTable />
-			</Provider>
-		);
-		expect(screen.queryByText(/Rocket/)).toBeNull();
-		expect(screen.queryByText(/Mission/)).toBeTruthy();
-	});
-	it("renders correctly", () => {
-		const tree = renderer
-			.create(
-				<Provider store={store}>
-					<MissionsTable />
-				</Provider>
-			)
-			.toJSON();
-		expect(tree).toMatchSnapshot();
-	});
+describe('Mission Element Tests', () => {
+  test('Check if Component Renders in DOM', () => {
+    render(
+      <Provider store={store}>
+        <MissionsTable />
+      </Provider>,
+    );
+    expect(screen.queryByText(/Rocket/)).toBeNull();
+    expect(screen.queryByText(/Mission/)).toBeTruthy();
+  });
+  it('renders correctly', () => {
+    const tree = renderer
+      .create(
+        <Provider store={store}>
+          <MissionsTable />
+        </Provider>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
--- src/tests/Rocket.test.js
+++ src/tests/Rocket.test.js
@@ -2,25 +2,25 @@ import React from 'react';
 import renderer from 'react-test-renderer';
 import { render, screen } from '@testing-library/react';
 import { Provider } from 'react-redux';
-import store from '../../src/redux/configureStore'
-import Rockets from '../../src/pages/Rockets';
+import store from '../redux/configureStore';
+import Rockets from '../pages/Rockets';
 
 describe('Rocket Element Tests', () => {
   test('Rocket Element renders correctly', () => {
     render(
       <Provider store={store}>
         <Rockets />
-      </Provider>
+      </Provider>,
     );
     expect(screen.queryByText(/Missions/)).toBeNull();
-		expect(screen.queryByText(/Description/)).toBeNull();
+    expect(screen.queryByText(/Description/)).toBeNull();
   });
   it('renders correctly', () => {
     const tree = renderer
       .create(
         <Provider store={store}>
           <Rockets />
-        </Provider>
+        </Provider>,
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
